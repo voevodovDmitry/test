93932132f0d91c66d6c2ff2ba3d95c8438636a7f
dmitryVoevodov <mersisay@yandex.ru>
2019-10-14 19:21:42 +0300

    commit for List_Users

--- index.js
+++ index.js
@@ -1,2 +1,57 @@
-console.log("111111111111111111111111") 
-console.log("Попыткка сохранить изменения череж дескопное приложение") 
+const users = [
+
+
+    {
+    id: 1,
+    name: 'Jon',
+    surname: 'Smith',
+    img: '#',
+    age: 18,
+    gender: 'male',
+    },
+
+    
+
+    {
+        id: 2,
+        name: 'Clarke',
+        surname: 'Henderson',
+        img: '#',
+        age: 33,
+        gender: 'male',
+    },
+
+    {
+        id: 3,
+        name: 'Rahim',
+        surname: 'Sterling',
+        img: '#',
+        age: 23,
+        gender: 'male',
+    },
+
+    {
+        id: 4,
+        name: 'Lasana',
+        surname: 'Traure',
+        img: '#',
+        age: 21,
+        gender: 'female',
+    },
+
+    {
+        id: 5,
+        name: 'Stiven',
+        surname: 'Gerard',
+        img: '#',
+        age: 41,
+        gender: 'male',
+    }
+
+
+]   
+
+for (let i=0; i<users.length; i++)
+{
+    console.log(users[i]);
+}
